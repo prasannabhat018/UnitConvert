b5c31b144b6bfe1c63c2b86705e9b6bd64595b73
Prasanna Bhat <prasannabhat018@gmail.com>
2024-07-28 15:14:44 +0530

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Prasanna Bhat on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
